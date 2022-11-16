87935ad3a60b97710a2f679c72b40a72d2e64796
Insha k n <55205426+inshakn@users.noreply.github.com>
2022-11-16 15:58:32 +0530

    Add files via upload
    Integer to Roman numbers

---
+++ Int_to_Roman.groovy
@@ -0,0 +1,17 @@
+
+       def intToRoman (int number) {
+       def rom_map = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+       def int_val  = [1000, 900, 500, 400,  100,   90,  50, 40,   10,    9,   5,   4,    1]
+        String roman="";
+        
+        for (int i = 0; i < rom_map.size(); i++) {
+            while (number >= int_val[i]) {
+            
+                number -= int_val[i];
+                roman  += rom_map[i];
+            }
+        }
+        return roman;
+    }  
+  a=  intToRoman(7)
+   println(a)
