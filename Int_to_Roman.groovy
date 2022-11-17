5f198dae114d19eb1be454526d09f2ccb867373d
Insha k n <55205426+inshakn@users.noreply.github.com>
2022-11-17 14:29:10 +0530

    Update Int_to_Roman.groovy

--- Int_to_Roman.groovy
+++ Int_to_Roman.groovy
@@ -1,20 +1,25 @@
-       import groovy.transform.BaseScript
-       @BaseScript hello helo
+       
        def intToRoman (int number) {
        def rom_map = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
        def int_val  = [1000, 900, 500, 400,  100,   90,  50, 40,   10,    9,   5,   4,    1]
-        String roman="";
+       String roman="";
         
         for (int i = 0; i < rom_map.size(); i++) {
-            while (number >= int_val[i]) {
+                                                        while (number >= int_val[i]) {
             
-                number -= int_val[i];
-                roman  += rom_map[i];
-            }
-        }
-        return roman;
+                                                                                               number -= int_val[i];
+                                                                                               roman  += rom_map[i];
+                                                                                      }
+                                                  }
+         return roman;
     }  
-  a=  intToRoman(7)
-   println(a)
+       
+a=  intToRoman(7)
+println(a)
 
-hello() // calling hello.groovy
+GroovyShell shell = new GroovyShell()
+def tools = shell.parse(new File('hello.groovy'))
+tools.hello()
+
+
+
