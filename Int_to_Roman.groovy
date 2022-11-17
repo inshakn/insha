b42bd1c596cb1c1a4c801b62869ab754ac8eb3f1
Insha k n <55205426+inshakn@users.noreply.github.com>
2022-11-17 14:56:02 +0530

    Update Int_to_Roman.groovy

--- Int_to_Roman.groovy
+++ Int_to_Roman.groovy
@@ -1,4 +1,7 @@
-       
+       GroovyShell shell = new GroovyShell()
+       def tools = shell.parse(new File('/var/jenkins_home/workspace/insha_git1/hello.groovy'))
+       tools.hey()
+
        def intToRoman (int number) {
        def rom_map = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
        def int_val  = [1000, 900, 500, 400,  100,   90,  50, 40,   10,    9,   5,   4,    1]
@@ -18,9 +21,6 @@ a=  intToRoman(78)
 println("78 in roman is ")
 print(a)
 
-GroovyShell shell = new GroovyShell()
-def tools = shell.parse(new File('/var/jenkins_home/workspace/insha_git1/hello.groovy'))
-tools.hey()
 
 
 
