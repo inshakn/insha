1216403d62a8f5a5b30626bdc45709f4bbd744bd
Insha k n <55205426+inshakn@users.noreply.github.com>
2022-11-17 14:09:41 +0530

    Update Int_to_Roman.groovy

--- Int_to_Roman.groovy
+++ Int_to_Roman.groovy
@@ -1,4 +1,5 @@
        import groovy.transform.BaseScript
+       @BaseScript hello hello
        def intToRoman (int number) {
        def rom_map = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
        def int_val  = [1000, 900, 500, 400,  100,   90,  50, 40,   10,    9,   5,   4,    1]
