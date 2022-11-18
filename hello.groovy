636e7e193ed04647baad9853d092e09b69289abf
Insha k n <55205426+inshakn@users.noreply.github.com>
2022-11-18 09:47:20 +0530

    Update hello.groovy

--- hello.groovy
+++ hello.groovy
@@ -1,10 +1,2 @@
-import com.cloudbees.groovy.cps.NonCPS
-@NonCPS
-def hey()
-{
-     def s = "hello, this is second file"
-     //println("Hello, this is second file")
-     return s;
-}
-
-//hey()
+a=  intToRoman(89)
+   println(a)
