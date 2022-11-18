f82ac098873c458bb6c6ca2293e765407934e811
Insha k n <55205426+inshakn@users.noreply.github.com>
2022-11-18 14:17:02 +0530

    Update Int_to_Roman.groovy

--- Int_to_Roman.groovy
+++ Int_to_Roman.groovy
@@ -1,6 +1,6 @@
-      import com.cloudbees.groovy.cps.NonCPS
+   //   import com.cloudbees.groovy.cps.NonCPS
 
-      @NonCPS
+   //   @NonCPS
        def intToRoman (int number) {
        def rom_map = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
        def int_val  = [1000, 900, 500, 400,  100,   90,  50, 40,   10,    9,   5,   4,    1]
